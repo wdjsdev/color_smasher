73c778b52ccd72a227e79ed01ceb3f21ee3d08ca
william <will.dowling@boombah.com>
2018-02-01 09:23:01 -0600

    ran jsFormat to format the code. no substantive changes

--- Color_Smasher_Dev.jsx
+++ Color_Smasher_Dev.jsx
@@ -138,7 +138,7 @@ function container()
 			valid = true;
 			log.l("Label color swatch successfully set to Info B.");
 		}
-		catch(e)
+		catch (e)
 		{
 			log.l("Info B did not exist. Creating a new swatch.");
 			var newSwatch = docRef.spots.add();
@@ -158,7 +158,7 @@ function container()
 			log.l("Successfully created Info B swatch.");
 		}
 
-		if(!valid)
+		if (!valid)
 		{
 			log.e("Failed while trying to create an Info B swatch.");
 			errorList.push("Failed while setting 'Info B' color.\nTry manually adding the 'Info B' swatch to the document swatches panel and try again.\nOtherwise, please restart Illustrator.");
@@ -168,8 +168,6 @@ function container()
 
 		return result;
 	}
-
-
 
 
 
@@ -184,19 +182,19 @@ function container()
 			var theLayer = layers["BKGRD, do not unlock"];
 			log.l("Set theLayer to BKGRD, do not unlock");
 		}
-		catch(e)
+		catch (e)
 		{
 			//set theLayer to the bottom-most layer
-			var theLayer = layers[layers.length-1];
+			var theLayer = layers[layers.length - 1];
 			log.l("No BKGRD layer present. Using the bottom layer which is " + theLayer.name);
 		}
 
 		theLayer.locked = false;
 		theLayer.visible = true;
-		for(var L=theLayer.groupItems.length-1;L>-1;L--)
+		for (var L = theLayer.groupItems.length - 1; L > -1; L--)
 		{
 			var curGroup = theLayer.groupItems[L];
-			if(curGroup.name.indexOf("Swatches")>-1)
+			if (curGroup.name.indexOf("Swatches") > -1)
 			{
 				curGroup.remove();
 			}
@@ -211,10 +209,10 @@ function container()
 	//Create/remove in progress indicator on the current artboard
 	//This helps the artist know whether they need to continue undoing in the event of a runtime error.
 	//When the artist executes enough undos, all indicators will be gone.
-	function inProgIndicator(bool,index)
+	function inProgIndicator(bool, index)
 	{
 		log.h("Beginning execution of inProgIndicator function with arguments: ::=>bool = " + bool + "::=>index = " + index);
-		if(bool)
+		if (bool)
 		{
 			log.l("Adding the inprogress indicator.");
 			var aB = artboards[index];
@@ -248,7 +246,7 @@ function container()
 	function setPrintable(bool)
 	{
 		log.h("Beginning execution of setPrintable function. Setting all layers to printable = " + bool);
-		for(var p=0;p<layers.length;p++)
+		for (var p = 0; p < layers.length; p++)
 		{
 			layers[p].printable = bool;
 		}
@@ -267,15 +265,15 @@ function container()
 		var inkLayer;
 		try
 		{
-			log.l("An ink layer already exists.");
 			inkLayer = layers["Ink Layer"];
+			log.l("An ink layer already exists.");
 			inkLayer.remove();
 			inkLayer = layers.add();
 			inkLayer.name = "Ink Layer";
 			inkLayer.printable = true;
 			log.l("Removed existing ink layer and created a new fresh one.");
 		}
-		catch(e)
+		catch (e)
 		{
 			log.l("No ink layer exists.");
 			inkLayer = layers.add();
@@ -291,10 +289,10 @@ function container()
 
 	//duplicateArt Function Description:
 	//duplicate selected artwork (all unlocked artwork on active artboard) to the Ink Layer
-	function duplicateArt(sel,lay)
+	function duplicateArt(sel, lay)
 	{
 		log.l("Beginning execution of duplicateArt function.");
-		for(var d=0;d<sel.length;d++)
+		for (var d = 0; d < sel.length; d++)
 		{
 			sel[d].duplicate(lay);
 		}
@@ -313,7 +311,7 @@ function container()
 		log.l("The following colors are in the inkList.::" + inkList.join("::"));
 		var trueColors = [];
 
-		if(template)
+		if (template)
 		{
 			log.l("File is a script template. Set the undesirable colors to the template undesirables array.");
 			undesirable = library.undesirables.template;
@@ -323,19 +321,19 @@ function container()
 			log.l("File is not a template. Set the undesirable colors to the non-template undesirables array.");
 			undesirable = library.undesirables.nonTemplate;
 		}
-		for(var i=inkList.length-1;i>-1;i--)
+		for (var i = inkList.length - 1; i > -1; i--)
 		{
 			var dontUse = false;
 			var thisInk = inkList[i];
-			if(thisInk.inkInfo.printingStatus == InkPrintStatus.DISABLEINK)
+			if (thisInk.inkInfo.printingStatus == InkPrintStatus.DISABLEINK)
 			{
 				log.l(thisInk + " is disabled. Skipping this color.");
 				// inkList.splice(i,1);
 				continue;
 			}
-			for(var u=0;u<undesirable.length;u++)
+			for (var u = 0; u < undesirable.length; u++)
 			{
-				if(thisInk.name.toLowerCase() == undesirable[u])
+				if (thisInk.name.toLowerCase() == undesirable[u])
 				{
 					// inkList.splice(i,1);
 					log.l(thisInk.name + " is undesirable. Skipping this color.");
@@ -345,37 +343,37 @@ function container()
 			}
 
 			//this swatch is printable and is not undesirable
-			if(!dontUse)
+			if (!dontUse)
 			{
 				log.l(thisInk + " is printable and not undesirable. Pushing it to the trueColors array.");
 				trueColors.push(inkList[i].name);
-				if(inkList[i].name == "Navy B")
+				if (inkList[i].name == "Navy B")
 				{
 					library.navyGray.navy = true;
 				}
-				else if(inkList[i].name == "Navy 2 B")
+				else if (inkList[i].name == "Navy 2 B")
 				{
 					library.navyGray.navy2 = true;
 				}
-				else if(inkList[i].name == "Gray B")
+				else if (inkList[i].name == "Gray B")
 				{
 					library.navyGray.gray = true;
 				}
-				else if(inkList[i].name == "Gray 2 B")
+				else if (inkList[i].name == "Gray 2 B")
 				{
 					library.navyGray.gray2 = true;
 				}
 			}
 		}
 
-		if(library.navyGray.navy && library.navyGray.navy2)
+		if (library.navyGray.navy && library.navyGray.navy2)
 		{
 			log.e("File contains Navy B and Navy 2 B.");
 			errorList.push("You have 'Navy B' AND 'Navy 2 B' in your mockup. Please undo, merge them, and try again.");
 			trueColors = null;
 
 		}
-		else if(library.navyGray.gray && library.navyGray.gray2)
+		else if (library.navyGray.gray && library.navyGray.gray2)
 		{
 			log.e("File contains Gray B and Gray 2 B.");
 			errorList.push("You have 'Gray B' AND 'Gray 2 B' in your mockup. Please undo, merge them, and try again.");
@@ -389,18 +387,18 @@ function container()
 
 	//makeColorChips Function Description
 	//take array of colors for the current artboard and create one color chip for each
-	function makeColorChips(colors,aB,dest)
+	function makeColorChips(colors, aB, dest)
 	{
 		log.h("Beginning execution of makeColorChips function.");
 		var height = aB.artboardRect[1] - aB.artboardRect[3];
 		var relativeVerticalPlacement = aB.artboardRect[3] + (height * .062);
-		
+
 
 		dest.locked = false;
 
 
 		var chipGroup = dest.groupItems.add();
-		chipGroup.name = "Swatches for Artboard " + (a+1);
+		chipGroup.name = "Swatches for Artboard " + (a + 1);
 
 		//determine chip width. each chip should be 1/8th the width of the document;.
 		var chipWidth = aB.artboardRect[2] - aB.artboardRect[0];
@@ -417,16 +415,16 @@ function container()
 		var swatchCounter = 1;
 
 		//loop each color and create a chip and label
-		for(var m=colors.length-1;m>-1;m--)
+		for (var m = colors.length - 1; m > -1; m--)
 		{
 			var curSwatch = colors[m];
-			if(curSwatch.toLowerCase().indexOf("process")==-1)
+			if (curSwatch.toLowerCase().indexOf("process") == -1)
 			{
 				try
 				{
 					var blahSwatch = swatches[curSwatch];
 				}
-				catch(e)
+				catch (e)
 				{
 					//this swatch doesn't exist.. skip it.
 					log.e(curSwatch + " does not exist in the swatches panel. Skipping this color.")
@@ -438,9 +436,9 @@ function container()
 			var tint = 0;
 
 			//determine tint of label color
-			for(var t=0;t<library.lightSwatches.length;t++)
+			for (var t = 0; t < library.lightSwatches.length; t++)
 			{
-				if(curSwatch.toLowerCase() == library.lightSwatches[t])
+				if (curSwatch.toLowerCase() == library.lightSwatches[t])
 				{
 					//this swatch is light colored
 					//set the label to display black so the label is still legible.
@@ -454,27 +452,27 @@ function container()
 
 			//Create background box that holds the fill color of the chip.
 
-			if(swatchCounter == 8)
+			if (swatchCounter == 8)
 			{
 				x = aB.artboardRect[0] - chipWidth + 5;
 				y = relativeVerticalPlacement - 20;
 			}
-			else if(swatchCounter == 15)
+			else if (swatchCounter == 15)
 			{
 				x = aB.artboardRect[0] - chipWidth + 5;
 				y = relativeVerticalPlacement - 40;
 			}
-			else if(swatchCounter == 22)
+			else if (swatchCounter == 22)
 			{
 				x = aB.artboardRect[0] - chipWidth + 5;
 				y = relativeVerticalPlacement - 60;
 			}
-			else if(swatchCounter >= 29)
+			else if (swatchCounter >= 29)
 			{
 				log.l("There are " + swatchCounter + " swatches in the document. This will likely cause issues with the RIP. User has been notified.");
 				errorList.push("You probably have too many swatches in this document.\nThere may be issues with the RIP software with this many swatches..");
 			}
-			var colorBox = thisGroup.pathItems.rectangle(y,x+=chipWidth,chipWidth,chipHeight);
+			var colorBox = thisGroup.pathItems.rectangle(y, x += chipWidth, chipWidth, chipHeight);
 
 
 			// if(chipGroup.groupItems.length < 8)
@@ -496,7 +494,7 @@ function container()
 			// 	var colorBox = thisGroup.pathItems.rectangle(y,x+=chipWidth,chipWidth,chipHeight);
 			// }
 
-			if(curSwatch.indexOf("Process")>-1)
+			if (curSwatch.indexOf("Process") > -1)
 			{
 				log.l(curSwatch + " is a process color. Setting fill to false and label color to black.");
 				colorBox.filled = false;
@@ -508,7 +506,7 @@ function container()
 				colorBox.filled = true;
 				colorBox.fillColor = swatches.getByName(curSwatch).color;
 			}
-			if(curSwatch != "White B")
+			if (curSwatch != "White B")
 			{
 				colorBox.stroked = false;
 			}
@@ -519,24 +517,24 @@ function container()
 				colorBox.strokeColor = labelColor.color;
 				colorBox.strokeWidth = 0.3;
 			}
-			
+
 			//Create a box to hold area text and a textFrame inside that box.
-			var textBox = thisGroup.pathItems.rectangle(y-6,x+5,chipWidth-10,15); //no stroke/no fill container to hold the dimensions of the textFrame
+			var textBox = thisGroup.pathItems.rectangle(y - 6, x + 5, chipWidth - 10, 15); //no stroke/no fill container to hold the dimensions of the textFrame
 			textBox.filled = false;
 			textBox.stroked = false;
 			var textRefBox = thisGroup.textFrames.areaText(textBox); //Text frame that holds the name of the swatch
 			textRefBox.contents = curSwatch;
 			textRefBox.textRange.fillColor = labelColor.color;
 			textRefBox.textRange.fillColor.tint = tint;
-			if(textRefBox.textRange.characters.length > 8 && textRefBox.textRange.characters.length <= 15)
+			if (textRefBox.textRange.characters.length > 8 && textRefBox.textRange.characters.length <= 15)
 			{
 				textRefBox.textRange.characterAttributes.horizontalScale = 75;
 			}
-			else if(textRefBox.textRange.characters.length > 15 && textRefBox.textRange.characters.length <= 20)
+			else if (textRefBox.textRange.characters.length > 15 && textRefBox.textRange.characters.length <= 20)
 			{
 				textRefBox.textRange.characterAttributes.horizontalScale = 55;
 			}
-			else if(textRefBox.textRange.characters.length > 20)
+			else if (textRefBox.textRange.characters.length > 20)
 			{
 				textRefBox.textRange.characterAttributes.horizontalScale = 45;
 			}
@@ -555,7 +553,7 @@ function container()
 	function getDest(template)
 	{
 		log.h("Beginning execution of getDest function.");
-		if(template)
+		if (template)
 		{
 			log.l("File is a template. Setting destLayer to BKGRD, do not unlock");
 			var destLayer = layers["BKGRD, do not unlock"];
@@ -568,7 +566,7 @@ function container()
 				var destLayer = layers["Artboard Swatches"];
 				log.l("Set destLayer to \"Artboard Swatches\"");
 			}
-			catch(e)
+			catch (e)
 			{
 				var destLayer = layers.add();
 				destLayer.name = "Artboard Swatches";
@@ -610,7 +608,7 @@ function container()
 		log.h("Beginning execution of unlockLayers function.");
 		var layLen = layers.length;
 		var thislay;
-		for(var l=0;l<layLen;l++)
+		for (var l = 0; l < layLen; l++)
 		{
 			thisLay = layers[l];
 			try
@@ -620,7 +618,7 @@ function container()
 				thisLay.layers["Information"].locked = true;
 				thisLay.layers["Prepress"].visible = false;
 			}
-			catch(e)
+			catch (e)
 			{
 				log.l("Looks like this isn't a template layer. moving on.");
 			}
@@ -639,30 +637,30 @@ function container()
 	////Data Storage////
 	////////////////////
 
-	var library = 
-	{
-		undesirables : 
+	var library = {
+		undesirables:
 		{
-			template :['jock tag b', 'thru-cut', 'info b', 'edge', 'cut line', 'cut', 'cutline', 'sewline', 'sew', 'sew line'],
-			nonTemplate : ['process cyan', 'process magenta', 'process yellow', 'process black', 'cut line', 'cut', 'edge', 'edge1', 'edge2', 'deleted global color',//
-											'deleted golbal color 1', 'deleted global color 2', 'deleted global color 3', 'deleted global color 4', 'deleted global color 5', //
-											'thru-cut', 'sew line', 'sewline', 'info b', 'jock tag b']
+			template: ['jock tag b', 'thru-cut', 'info b', 'edge', 'cut line', 'cut', 'cutline', 'sewline', 'sew', 'sew line'],
+			nonTemplate: ['process cyan', 'process magenta', 'process yellow', 'process black', 'cut line', 'cut', 'edge', 'edge1', 'edge2', 'deleted global color', //
+				'deleted golbal color 1', 'deleted global color 2', 'deleted global color 3', 'deleted global color 4', 'deleted global color 5', //
+				'thru-cut', 'sew line', 'sewline', 'info b', 'jock tag b'
+			]
 		},
 
-		lightSwatches : ["twitch b", "flo yellow b", "dark flesh b", "flesh b", "soft pink b", "vegas gold b", "optic yellow b", "yellow b", "lime green b", "white b", "gray b", "gray 2 b"],
-		navyGray : 
+		lightSwatches: ["twitch b", "flo yellow b", "dark flesh b", "flesh b", "soft pink b", "vegas gold b", "optic yellow b", "yellow b", "lime green b", "white b", "gray b", "gray 2 b"],
+		navyGray:
 		{
-			navy : false,
-			navy2 : false,
-			gray : false,
-			gray2 : false
+			navy: false,
+			navy2: false,
+			gray: false,
+			gray2: false
 		}
 	}
 
 
-	if(user == "will.dowling")
+	if (user == "will.dowling")
 	{
-		logDest.push(new File("~/Desktop/automation/javascript/logging/color_smasher_dev_log.txt"));
+		logDest.push(new File("~/Desktop/automation/logs/color_smasher_dev_log.txt"));
 	}
 	else
 	{
@@ -679,7 +677,7 @@ function container()
 	///Function Calls///
 	////////////////////
 
-	if(!valid)
+	if (!valid)
 	{
 		return false;
 	}
@@ -693,14 +691,13 @@ function container()
 	var scriptNotes = [];
 
 
-
 	var template = isTemplate(docRef);
 
 	var destLayer = getDest(template);
 
 	var labelColor = getLabelColor();
 
-	if(labelColor == null)
+	if (labelColor == null)
 	{
 		sendErrors(errorList);
 		return;
@@ -714,11 +711,11 @@ function container()
 	setPrintable(false);
 
 	//loop each artboard and generate chips for each individual artboard
-	for(var a=0;a<artboards.length;a++)
+	for (var a = 0; a < artboards.length; a++)
 	{
-		inProgIndicator(true,a);
-
-		if(template)
+		inProgIndicator(true, a);
+		
+		if (template)
 		{
 			// lockUnlockLayers();
 			unlockLayers();
@@ -732,29 +729,28 @@ function container()
 		docRef.selectObjectsOnActiveArtboard();
 
 		//duplicate all of the selected artwork to the Ink Layer
-		duplicateArt(docRef.selection,inkLayer);
+		duplicateArt(docRef.selection, inkLayer);
 
 		var colors = generateInkList();
 
-		if(colors == null)
+		if (colors == null)
 		{
 			sendErrors(errorList);
 			return;
 		}
 
 
-		if(colors.length<1)
+		if (colors.length < 1)
 		{
-			errorList.push("No Boombah colors were identified on artboard " + (a+1) + "...?");
+			errorList.push("No Boombah colors were identified on artboard " + (a + 1) + "...?");
 		}
 
 
-		makeColorChips(colors,artboards[a],destLayer);
+		makeColorChips(colors, artboards[a], destLayer);
 
 
-		
 
-		if(inkLayer != undefined)
+		if (inkLayer != undefined)
 			inkLayer.remove();
 		inProgIndicator(false);
 	}
@@ -764,7 +760,7 @@ function container()
 
 	docRef.selection = null;
 
-	if(errorList.length>0)
+	if (errorList.length > 0)
 		sendErrors(errorList);
 
 	printLog();
